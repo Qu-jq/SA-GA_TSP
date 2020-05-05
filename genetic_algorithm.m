aec47bc6f2399bacf0524a3e2fd9a49dbec8978a
Qu-jq <1587743243@qq.com>
2020-05-05 16:47:03 +0800

    init repository

---
+++ genetic_algorithm.m
@@ -0,0 +1,130 @@
+function f = genetic_algorithm(D, mute, Pm)
+% 遗传算法解决中国省会城市TSP问题的简单实现
+% 包含台北，香港，澳门
+clear;
+if ~exist('mute', 'var')
+    mute = 0; % 是否显示各种提示信息
+end
+if ~exist('Pm', 'var')
+    Pm = 0.2; % 变异概率，越大收敛越慢但是解一般越好
+end
+if ~exist('D', 'var')
+   data=xlsread('省会经纬坐标.xlsx');
+    C=data;
+    D=zeros(35,35);% D为城市间的距离矩阵
+    % 形成两两之间对应的矩阵（对称阵，可以只看上三角或下三角）
+    [LA1,LA2]=meshgrid(C(:,2));
+    [LO1,LO2]=meshgrid(C(:,1));
+
+    % 计算两两之间的距离，单位为公里
+    R = distance(LA1,LO1,LA2,LO2,almanac('earth','wgs84'));
+    D = num2str(R,'%10.2f');
+    %disp(D);
+end
+rng(1);
+n = size(D, 1);
+N = 100; % 群体规模
+TOL = 20; % 最大容忍次数(连续TOL次rate不上升，或找不到更优解，则停止迭代)
+
+solutions = zeros(N, n);
+fs = zeros(N, 1);
+for i = 1:N
+    solutions(i, :) = [1, randperm(n-1) + 1]; % 生成N个解，假定从1开始
+    fs(i) = TSP_distance(D, solutions(i, :));
+end
+Pu = max(fs) - fs + 1;
+P = Pu/sum(Pu);
+cumP = cumsum(P);
+best = min(fs);
+avg = mean(fs);
+rate = best/avg;
+if ~mute
+disp('初始解的群体中最短的路径长度为：');
+disp(best);
+disp('初始解的群体中平均路径长度为：');
+disp(avg);
+end
+
+tol = 0;
+count = 0;
+while 1
+    count = count + 1;
+    if ~mute
+        fprintf('当前第%d次迭代\n', count);
+    end
+    parents = zeros(size(solutions));
+    for i = 1:N % 使用轮盘赌的方式选出父代的染色体
+        index = sum(cumP <= rand) + 1;
+        parents(i, :) = solutions(index, :);
+    end
+    new_solutions = zeros(size(solutions));
+    assert(mod(N, 2) == 0);
+    for i = 1:N/2 % 交配操作；这里默认N为偶数，每两个父代一起产生两个子代
+        % 产生的子代1取父代1的前一半染色体，后一半则由父代2提供；同理于子代2
+        p1 = parents(2*i-1, :);
+        p2 = parents(2*i, :);
+        middle = ceil(n/2);
+        s1 = p1(1:middle);
+        res1 = setdiff(p2, s1, 'stable');
+        s1 = [s1, res1];
+        s2 = p2(1:middle);
+        res2 = setdiff(p1, s2, 'stable');
+        s2 = [s2, res2];
+        new_solutions(2*i-1, :) = s1;
+        new_solutions(2*i, :) = s2;
+    end
+    for i = 1:N % 变异操作；变异的方式为随机取两个城市的顺序交换
+        if rand < Pm
+            temp = randperm(n-1) + 1;
+            k = temp(1);
+            new_solutions(i, [1, k]) = new_solutions(i, [k, 1]);
+        end
+    end
+    % 至此，新的种群已经生成完毕，代替旧种群后开始新一轮的计算
+    solutions = new_solutions;
+    for i = 1:N
+        fs(i) = TSP_distance(D, solutions(i, :));
+    end
+    Pu = max(fs) - fs + 1;
+    P = Pu/sum(Pu);
+    cumP = cumsum(P);
+    best_new = min(fs);
+    avg = mean(fs);
+    rate_new = best_new/avg;
+    if ~mute
+        disp('最短的路径长度为：');
+        disp(best_new);
+        disp('平均的路径长度为：');
+        disp(avg);
+    end
+    tol = tol + 1;
+    if best_new < best || rate_new > rate
+        best = best_new;
+        rate = rate_new;
+        tol = 0;
+    end
+    if tol >= TOL
+        break
+    end
+    if count > 5000
+        break
+    end
+end
+[f, index] = min(fs);
+solution = solutions(index, :);
+if ~mute
+fprintf('最后搜索得到的最优路径为：\n');
+disp(solution);
+disp('路径长度为：');
+disp(f);
+end
+
+function f = TSP_distance(D, solution)
+% 本函数计算给定solution的距离，其中城市之间的距离由D给出。
+n = numel(solution);
+sum = 0;
+for i = 1:n-1
+    sum = sum + D(solution(i), solution(i+1));
+end
+sum = sum + D(solution(n), solution(1));
+f = sum;
